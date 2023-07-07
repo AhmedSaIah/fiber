7a18a0fa1012ad069fc27b98a2b71c908ea31241
AhmedSaIah <ahmedsalah5446@gmail.com>
2023-07-07 15:41:08 +0300

    user repo wip

--- repository/User.go
+++ repository/User.go
@@ -1,46 +1,62 @@
-package database
+package repository
 
 import (
 	"context"
 	"fmt"
-
+	"github.com/AhmedSaIah/fiber/database"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/AhmedSaIah/fiber/models"
 )
 
-type UserRepository struct {
+const UsersCollection = "users"
+
+type UserRepository interface {
+	Save(user *models.User) error
+	//Update(user *models.User) error
+	//GetById(id string) (user *models.User, err error)
+	GetByEmail(email string) (user *models.User, err error)
+	//GetAll() (users []*models.User, err error)
+	//Delete(id string) error
+}
+
+type userRepository struct {
 	collection *mongo.Collection
 }
 
-func NewUserRepository(client *mongo.Client) *UserRepository {
-	return &UserRepository{
-		collection: client.Database("fiber").Collection("users"),
-	}
+func NewUserRepository(conn database.Connection) UserRepository {
+	return &userRepository{conn.DB().Collection(UsersCollection)}
 }
 
-func (u *UserRepository) Save(user *models.User) error {
+func (u *userRepository) Save(user *models.User) error {
 	_, err := u.collection.InsertOne(context.Background(), user)
 	if err != nil {
-		fmt.Errorf("error creating user: %w", err)
+		return err
 	}
-	return err
+	return nil
 }
 
-func (u *UserRepository) FindByEmail(email string) (*models.User, error) {
-	var user models.User
-	err := u.collection.FindOne(context.Background(), bson.M{"email": email}).Decode(&user)
-
-	if err != nil {
+func (u *userRepository) GetByEmail(email string) (*models.User, error) {
+	filer := bson.M{"email": email}
+	opts := options.FindOne()
+	result := u.collection.FindOne(context.Background(), filer, opts)
+	if err := result.Err(); err != nil {
 		if err == mongo.ErrNoDocuments {
-			return nil, fmt.Errorf("error finding user with that email: %w", err)
+			return nil, fmt.Errorf("no user found with email provided %w", err)
 		}
+		return nil, err
+	}
+
+	var user models.User
+	if err := result.Decode(&user); err != nil {
+		return nil, err
 	}
 	return &user, nil
 }
 
-func (u *UserRepository) Delete(email string) error {
+func (u *userRepository) Delete(email string) error {
 	var user models.User
 	err := u.collection.FindOneAndDelete(context.Background(), bson.M{"email": email}).Decode(&user)
 
