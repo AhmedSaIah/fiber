22588ea03d9bdfea9140759b291d42c36475bda6
AhmedSaIah <ahmedsalah5446@gmail.com>
2023-07-07 15:40:38 +0300

    db wip

--- database/db.go
+++ database/db.go
@@ -2,25 +2,60 @@ package database
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ConnDB(databaseURL string) (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI(databaseURL)
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to DB: %w", err)
-	}
-
-	err = client.Ping(context.Background(), nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed pinging DB: %w", err)
-	}
-
-	log.Println("Connected to database successfully!")
-	return client, nil
+type Connection interface {
+	Close()
+	DB() *mongo.Database
 }
+
+type conn struct {
+	client *mongo.Client
+}
+
+func NewConnection() Connection {
+	var c conn
+	var err error
+	url := getURL()
+
+	clientOptions := options.Client().ApplyURI(url)
+
+	c.client, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	return &c
+}
+
+func (c *conn) Close() {
+	c.Close()
+}
+
+func (c *conn) DB() *mongo.Database {
+	return c.client.Database("fiber")
+}
+
+func getURL() string {
+	return os.Getenv("MONGO_URL")
+}
+
+//func ConnDB(databaseURL string) (*mongo.Client, error) {
+//	clientOptions := options.Client().ApplyURI(databaseURL)
+//	client, err := mongo.Connect(context.Background(), clientOptions)
+//	if err != nil {
+//		return nil, fmt.Errorf("failed to connect to DB: %w", err)
+//	}
+//
+//	err = client.Ping(context.Background(), nil)
+//	if err != nil {
+//		return nil, fmt.Errorf("failed pinging DB: %w", err)
+//	}
+//
+//	log.Println("Connected to database successfully!")
+//	return client, nil
+//}
